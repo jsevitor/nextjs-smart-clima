35ba9dbadb7d33276130a3af258da82fe399bc0d
jsevitor <vitorjseo@gmail.com>
2025-06-30 13:55:09 -0300

    feat: adciona dados da api para serem renderizados em MoonPhaseCard

--- src/app/components/weather/MoonPhaseCard.tsx
+++ src/app/components/weather/MoonPhaseCard.tsx
@@ -1,20 +1,38 @@
+import { useWeatherStore } from "@/store/useWeatherStore";
+import { moonPhases } from "@/utils/resources";
+
 export default function MoonPhaseCard() {
+  const { data, loading } = useWeatherStore();
+
+  const forecastData = data?.forecast;
+
+  if (loading) return <p>Carregando...</p>;
+
   return (
-    <div className="bg-borderColor w-1/3 rounded-2xl p-4 h-64">
-      <div className="flex flex-col justify-between gap-4 h-full">
-        <div className="flex justify-center border-b border-background pb-2">
-          <h2>Fase da Lua</h2>
+    <div className="bg-borderColor w-1/3 rounded-2xl h-80 p-4">
+      {data ? (
+        <div className="flex flex-col justify-between gap-4 h-full">
+          <div className="flex justify-center border-b border-background pb-2">
+            <h2>Fase da Lua</h2>
+          </div>
+          <div className="flex flex-col gap-6 justify-center items-center">
+            <i className="bi bi-moon-stars text-7xl"></i>
+            <span>{moonPhases[data.moon_phase]}</span>
+          </div>
+          <div className="flex justify-center gap-4">
+            {forecastData?.slice(0, 7).map((item, index) => (
+              <div className="flex flex-col items-center">
+                <i key={index} className="bi bi-moon-fill text-2xl"></i>
+                <span className="text-xs text-center">
+                  {moonPhases[item.moon_phase]}
+                </span>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="flex flex-col gap-4 justify-center items-center">
-          <i className="bi bi-moon-stars text-5xl"></i>
-          <p>Lua Nova</p>
-        </div>
-        <div className="flex justify-center gap-4">
-          {Array.from({ length: 7 }, (_, index) => (
-            <i key={index} className="bi bi-moon-fill text-2xl"></i>
-          ))}
-        </div>
-      </div>
+      ) : (
+        <p>Erro ao carregar</p>
+      )}
     </div>
   );
 }
---
+++ src/utils/resources.ts
@@ -0,0 +1,20 @@
+export const weekdays = {
+  Seg: "Segunda-feira",
+  Ter: "Terça-feira",
+  Qua: "Quarta-feira",
+  Qui: "Quinta-feira",
+  Sex: "Sexta-feira",
+  Sab: "Sábado",
+  Dom: "Domingo",
+};
+
+export const moonPhases: Record<string, string> = {
+  new_moon: "Lua Nova",
+  waxing_crescent: "Lua Crescente",
+  first_quarter: "Quarto Crescente",
+  waxing_gibbous: "Crescente Gibbosa",
+  full: "Lua Cheia",
+  waning_gibbous: "Minguante Gibbosa",
+  last_quarter: "QuartoMinguante",
+  waning_crescent: "Lua Minguante",
+};
